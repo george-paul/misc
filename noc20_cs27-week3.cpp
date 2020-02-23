f46755f24dabac8b775b6c42674f1ac173e6ee9c
George Paul <g.paul759@gmail.com>
2020-02-23 20:02:49 +0530

    Road Trips

--- noc20_cs27-week3.cpp
+++ noc20_cs27-week3.cpp
@@ -2,6 +2,7 @@
 #include <cstdio>
 #include <vector>
 #include <algorithm>
+#include <queue>
 
 #define DE(x) cout<<'>'<<#x<<':'<<x<<endl;
 
@@ -10,46 +11,122 @@ using namespace std;
 class City
 {
 public:
+	int no=0;
 	int mus=0;
+	int visited=0;
 	vector<int> adj{};
 };
 
-std::vector<char> v;
-if (FILE *fp = fopen("filename", "r"))
-{
-	char buf[1024];
-	while (size_t len = fread(buf, 1, sizeof(buf), fp))
-		v.insert(v.end(), buf, buf + len);
-	fclose(fp);
-}int N, M, K;
+int N, M, K;
 vector<City> cities;
+vector<City> Tcities;
 
-void sortCities(int lower, int upper)
+// void sortCities(vector<City> &Tcities, int lower, int upper)
+// {
+// 	if(upper<=lower)
+// 		return;
+// 	City temp;
+// 	int pivot=Tcities[upper].mus;
+// 	int lesser=lower, i;
+// 	for(i=lower; i<upper; i++)
+// 	{
+// 		if(Tcities[i].mus<pivot)
+// 		{
+// 			temp=Tcities[i];
+// 			Tcities[i]=Tcities[lesser];
+// 			Tcities[lesser]=temp;
+// 			lesser++;
+// 		}
+// 	}
+// 	temp=Tcities[lesser];
+// 	Tcities[lesser]=Tcities[upper];
+// 	Tcities[upper]=temp;
+// 	sortCities(Tcities,lower,lesser-1);
+// 	sortCities(Tcities,lesser+1, upper);
+// 	return;
+// }
+
+int MAXmus()
 {
-	if(upper<=lower)
-		return;
-	int pivot=cities[upper].mus;
-	int lesser=lower;
-	for(int i=1; i<upper; i++)
+	int maxpos=0;
+	for(int i=0; i<N; i++)
 	{
-		if(cities[i].mus<pivot)
+		if(cities[i].mus>cities[maxpos].mus && !(cities[i].visited))
 		{
-			swap(cities[i],cities[lesser]);
-			lesser++;
+			maxpos=i;
 		}
 	}
-	sortCities(lower,lesser-1);
-	sortCities(lesser, upper);
-	return;
+	return cities[maxpos].no;
 }
 
-void Fun()
+int MINmus()
 {
-	sortCities(0, N-1);
-	for(auto a:cities)
+	int maxpos=0;
+	for(int i=0; i<N; i++)
 	{
-		DE(a.mus);
+		if(cities[i].mus<cities[maxpos].mus && !(cities[i].visited))
+		{
+			maxpos=i;
+		}
 	}
+	return cities[maxpos].no;
+}
+
+int citiesUnvisited()
+{
+	for(int i=0; i<N; i++)
+	{
+		if(cities[i].visited==0)
+		{
+			return 1;
+		}
+	}
+	return 0;
+}
+
+int Month(int pos)
+{
+	queue<int> q;
+	int i, sum=cities[pos].mus;
+	q.push(pos);
+	cities[pos].visited=1;
+	while(!q.empty())
+	{
+		i=q.front();
+		q.pop();
+		for(auto j=cities[i].adj.begin(); j!=cities[i].adj.end(); j++)
+		{
+			int a=*j;
+			if(!cities[a].visited)
+			{
+				cities[a].visited=1;
+				sum+=cities[a].mus;
+				q.push(a);
+			}
+		}
+	}
+	return sum;
+}
+
+int Fun()
+{
+	int i, sumOfMuseums=0;
+	Tcities = cities;
+	for(i=0; i<K&&citiesUnvisited(); i++)
+	{
+		if(i%2==0)		//Lavanya
+		{
+			sumOfMuseums+=Month(MAXmus());
+		}
+		else			//Nikhil
+		{
+			sumOfMuseums+=Month(MINmus());
+		}
+	}
+	if(i<K)
+		return -1;
+	else
+		return sumOfMuseums;
 }
 
 int main()
@@ -59,14 +136,14 @@ int main()
 	City c;
 
 	cin>>N>>M>>K;
-	for (int i=0; i<N; ++i)
+	for (int i=0; i<N; i++)
 	{
 		cities.push_back(c);
 	}
 	for(i=0; i<M; i++)
 	{
 		cin>>u>>v;
-		cout<<u<<v<<endl;
+		u--;v--;
 		cities[u].adj.push_back(v);
 		cities[v].adj.push_back(u);
 	}
@@ -74,9 +151,10 @@ int main()
 	{
 		cin>>u;
 		cities[i].mus=u;
+		cities[i].no=i;
 	}
 
-	Fun();
+	cout<<Fun()<<'\n';
 
 	return 0;
 }
